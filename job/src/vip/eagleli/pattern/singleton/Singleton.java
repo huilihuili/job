b14abc8ec3d22c4354141b08eaaa8b5b4008cbd2
lh <lh@WIN-S2TF1S6NQ9T>
2019-07-05 12:35:28 +0800

    Singleton

---
+++ job/src/vip/eagleli/pattern/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package vip.eagleli.pattern.singleton;
+
+public class Singleton {
+	private Singleton() {
+
+	}
+
+	private static class SingletonHolder {
+		private static Singleton uniqueInstance;
+	}
+	
+	public static Singleton getUniqueInstance() {
+		return SingletonHolder.uniqueInstance;
+	}
+}
