e4379c3bc7809a434e4eb3ad5a4aeaba99875a67
lh <lh@WIN-S2TF1S6NQ9T>
2019-07-25 20:28:28 +0800

    heap sort

--- job/src/vip/eagleli/structure/sort/HeapSort.java
+++ job/src/vip/eagleli/structure/sort/HeapSort.java
@@ -8,9 +8,11 @@ public class HeapSort {
 		if (arr == null || arr.length < 2) {
 			return;
 		}
+
 		for (int i = 0; i < arr.length; i++) {
 			heapInsert(arr, i);
 		}
+
 		int size = arr.length;
 		swap(arr, 0, --size);
 		while (size > 0) {
