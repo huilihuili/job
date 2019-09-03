057667210573c3fbf1b9962229fdba62bd658439
lh <lh@WIN-S2TF1S6NQ9T>
2019-09-03 15:10:49 +0800

    中文的顺序

--- job/src/vip/eagleli/foundation/StringTest.java
+++ job/src/vip/eagleli/foundation/StringTest.java
@@ -1,8 +1,21 @@
 package vip.eagleli.foundation;
 
+import org.junit.Test;
+
 public class StringTest {
 	public static void main(String[] args) {
 		String str = null;
 		System.out.println(str);
+	}
+
+	@Test
+	public void compareToTest() {
+		String str1 = "ÄãºÃ°¡";
+		String str2 = "ÎÒÊÇË­";
+		for (int i = 0; i < str1.length(); i++) {
+			System.out.println(str1.charAt(i));
+			System.out.println(str2.charAt(i));
+			System.out.println(str1.charAt(i) - str2.charAt(i));
+		}
 	}
 }
