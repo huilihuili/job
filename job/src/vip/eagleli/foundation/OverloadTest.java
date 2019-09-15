3766f7aa10c9fa63050238419b33a13e4e0aedc4
lh <lh@WIN-S2TF1S6NQ9T>
2019-09-15 10:57:28 +0800

    泛型是否重载？不可以

---
+++ job/src/vip/eagleli/foundation/OverloadTest.java
@@ -0,0 +1,10 @@
+package vip.eagleli.foundation;
+
+public class OverloadTest {
+	
+	// ²»ÄÜÖØÔØ
+	// public void fun(List<String> list) {
+	// }
+	// public void fun(List<Integer> list) {
+	// }
+}
