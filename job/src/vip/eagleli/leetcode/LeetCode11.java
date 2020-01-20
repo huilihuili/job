0d40e8849d2c9f21fb4513d172975f011cd4a259
lh <lh@WIN-S2TF1S6NQ9T>
2020-01-20 11:00:06 +0800

    LeetCode 11

---
+++ job/src/vip/eagleli/leetcode/LeetCode11.java
@@ -0,0 +1,58 @@
+package vip.eagleli.leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode11 {
+	public static void main(String[] args) {
+		LeetCode11 leetCode11 = new LeetCode11();
+		int[] height = { 1, 8, 6, 10, 5, 4, 9, 3, 7 };
+		leetCode11.maxArea(height);
+		System.out.println("-------");
+		leetCode11.maxArea2(height);
+	}
+
+	/**
+	 * ±©Á¦·¨
+	 * 
+	 * @param height
+	 * @return
+	 */
+	public int maxArea(int[] height) {
+		int ans = 0;
+		for (int i = 0; i < height.length; i++) {
+			for (int j = 0; j < height.length; j++) {
+				ans = Math.max(ans, Math.min(height[i], height[j]) * (j - i));
+				System.out.print(Math.min(height[i], height[j]) * Math.abs(j - i) + "\t");
+			}
+			System.out.println();
+		}
+		return ans;
+	}
+
+	public int maxArea2(int[] height) {
+		int ans = 0, i = 0, j = height.length - 1;
+		char[][] cs = new char[height.length][height.length];
+		char step = '0';
+		while (i < j) {
+			ans = Math.max(ans, (j - i) * Math.min(height[i], height[j]));
+			cs[i][j] = step;
+			if (height[i] < height[j]) {
+				for (int k = i + 1; k < j; k++) {
+					cs[i][k] = step;
+				}
+
+				i++;
+			} else {
+				for (int k = i + 1; k < j; k++) {
+					cs[k][j] = step;
+				}
+				j--;
+			}
+			step++;
+		}
+		for (int k = 0; k < height.length; k++) {
+			System.out.println(Arrays.toString(cs[k]));
+		}
+		return ans;
+	}
+}
