986f0c75018635b39d4c6f950ab126dbe3ef5c8f
huilihuili <864814732@qq.com>
2020-03-05 11:26:46 +0800

    update LeetCode 52

--- job/src/vip/eagleli/leetcode/LeetCode52.java
+++ job/src/vip/eagleli/leetcode/LeetCode52.java
@@ -3,6 +3,8 @@ package vip.eagleli.leetcode;
 public class LeetCode52 {
 
 	public static void main(String[] args) {
+		LeetCode52 leetCode52 = new LeetCode52();
+		System.out.println(leetCode52.totalNQueens3(4));
 	}
 
 	public int totalNQueens(int n) {
@@ -84,6 +86,27 @@ public class LeetCode52 {
 
 	public void removeQueen(int row, int col) {
 		rows[col] = hills[row - col + n - 1] = dales[row + col] = false;
+	}
+
+	int res;
+
+	public int totalNQueens3(int n) {
+		dfs(n, 0, 0, 0, 0);
+		return res;
+	}
+
+	private void dfs(int n, int row, int col, int ld, int rd) {
+		if (row >= n) {
+			res++;
+			return;
+		}
+		// 将所有能放置 Q 的位置由 0 变成 1，以便进行后续的位遍历
+		int bits = ~(col | ld | rd) & ((1 << n) - 1);
+		while (bits > 0) {
+			int pick = bits & -bits; // 注: x & -x
+			dfs(n, row + 1, col | pick, (ld | pick) << 1, (rd | pick) >> 1);
+			bits &= bits - 1; // 注: x & (x - 1)
+		}
 	}
 
 }
