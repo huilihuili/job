b2ae23a794c2fa96f196309a642147832047e271
huilihuili <864814732@qq.com>
2020-04-01 09:22:33 +0800

    LeetCode 912

---
+++ job/src/vip/eagleli/leetcode/LeetCode912.java
@@ -0,0 +1,14 @@
+package vip.eagleli.leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode912 {
+	public static void main(String[] args) {
+
+	}
+
+	public int[] sortArray(int[] nums) {
+		Arrays.sort(nums);
+		return nums;
+	}
+}
